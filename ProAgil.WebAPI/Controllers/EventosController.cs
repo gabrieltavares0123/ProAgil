23465d6f87e3691d12fe03fbdde888ec30c65bbf
Gabriel Jorge <gabriel.tavaresramos@gmail.com>
2020-04-12 19:55:59 -0300

    Commit inicial.

---
+++ ProAgil.WebAPI/Controllers/EventosController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly ILogger<EventosController> _logger;
+        public DataContext Context { get; }
+
+        public EventosController(ILogger<EventosController> logger, DataContext context)
+        {
+            _logger = logger;
+            Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await Context.Eventos.ToListAsync<Evento>();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou.");
+            }
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await Context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou.");
+            }
+        }
+    }
+}
---
+++ ProAgil.WebAPI/Migrations/20200412223312_Evento_ImagemUrl_Correcao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class Evento_ImagemUrl_Correcao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "imagemUrl",
+                table: "Eventos",
+                newName: "ImagemUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagemUrl",
+                table: "Eventos",
+                newName: "imagemUrl");
+        }
+    }
+}
